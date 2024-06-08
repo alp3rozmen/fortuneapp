4889808f791eb49f75e37b03837470d1e04c9a3f
alp3rozmen <alp3rozmen@gmail.com>
2024-06-08 21:28:29 +0300

    falcilar düzenle cart curt

--- fortuneteller/src/views/dashboard/Default/index.js
+++ fortuneteller/src/views/dashboard/Default/index.js
@@ -1,11 +1,12 @@
 
 // material-ui
 import { Grid, Typography } from '@mui/material';
-
+import FortuneTelling from 'views/fortune-telling/index.js';
 // project imports
 
 
 import { gridSpacing } from 'store/constant';
+import MainCard from 'ui-component/cards/MainCard';
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
@@ -13,11 +14,14 @@ const Dashboard = () => {
   
 
   return (
-    <Grid container spacing={gridSpacing}>
-      <Grid item xs={12}>
-        <Typography variant="h1">Hoşgeldiniz</Typography>
-      </Grid>   
-    </Grid>
+    <MainCard>
+      <Grid container spacing={gridSpacing}>
+        <Grid item xs={12}>
+          <Typography variant="h3">Popüler Yorumcularımız</Typography>
+          <FortuneTelling />
+        </Grid>
+      </Grid>
+    </MainCard>
   );
 };
 
