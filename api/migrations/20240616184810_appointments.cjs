f2c73d05c02aa4d615ec763e587b8818d0f94f8d
alp3rozmen <alp3rozmen@gmail.com>
2024-06-17 23:57:43 +0300

    table fix

--- api/migrations/20240616184810_appointments.cjs
+++ api/migrations/20240616184810_appointments.cjs
@@ -7,8 +7,10 @@ return knex.schema.createTable('appointments', function(table) {
     table.increments('id').primary();
     table.integer('user_details_id').comment('ud_id').unsigned();
     table.foreign('user_details_id').references('id').inTable('user_details');
-    table.dateTime('app_start_date').comment('randevu_baslangic_gunu');
-    table.dateTime('app_end_date').comment('randevu_bitis_gunu');
+    table.date('app_start_date').comment('randevu_baslangic_gunu');
+    table.date('app_end_date').comment('randevu_bitis_gunu');
+    table.time('start_hour').comment('randevu_baslangic_saat');
+    table.time('end_hour').comment('randevu_bitis_saat');
     table.string('interval_time').comment('saat araligi');
     table.timestamps(true, true);
 });
