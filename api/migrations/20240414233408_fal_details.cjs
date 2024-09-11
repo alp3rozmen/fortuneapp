c2746699c98285b1b92d174cc5784a705a1c7be9
alp3rozmen <alp3rozmen@gmail.com>
2024-09-11 20:42:28 +0300

    fal_details_table fix

--- api/migrations/20240414233408_fal_details.cjs
+++ api/migrations/20240414233408_fal_details.cjs
@@ -8,15 +8,9 @@ exports.up = function(knex) {
     table.increments('id').primary();
     table.integer('fal_id').comment('fals_id').unsigned();
     table.foreign('fal_id').references('id').inTable('fals');
-    table.string('gender').comment('cinsiyet');
-    table.string('age').comment('yas');
-    table.string('status').comment('status');
-    table.string('city').comment('sehir');
-    table.string('country').comment('ulke');
-    table.string('type').comment('bakmakistedigi tip 1 ask 2 is 3 her ikiside 4 digerler');
-    table.binary('image1_url').comment('resim1');  //ilk fal resmi
-    table.binary('image2_url').comment('resim2');  //ikinci fal resmi
-    table.binary('image3_url').comment('resim3');  //ucuncu fal resmi
+    table.text('formdata').comment('formun tasarımı');
+    table.text('formanswer').comment('formun cevapları');
+    table.string('status').comment('status');  
     table.string('comment').comment('falyorumu');
     table.timestamps(true, true);
   });
